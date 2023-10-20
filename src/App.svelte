d4e9b8a854f84e099e39e3ec9ece2749226d4735
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-10-20 19:58:02 -0300

    Crear item, save/fetch localstorage, selection reactivity, list

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,60 @@
 <script lang="ts">
+  let inputItem: string = ''
+  let todos = JSON.parse(localStorage.getItem("todos") || "[{}]") || undefined
   function handleForm(e: any) {
     e.preventDefault()
-    console.log(e)
+    console.log(inputItem)
+    let newItem = {
+      id: Math.round(Math.random() * 12412),
+      name: inputItem,
+      completed: false
+    }
+    todos = [...todos, newItem]
+    console.log(todos)
+    inputItem = ""
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }
+  function selected(e: any) {
+    console.log(e.target.dataset.id)
+    let id = e.target.dataset.id
+    let element = document.querySelector(`#todo-${id}`)
+    element?.classList.toggle("completed-false")
+    element?.classList.toggle("completed-true")
   }
 </script>
 
 <main>
   <form on:submit={handleForm}>
     <label for="newitem">Nuevo item</label>
-    <input type="text" name="newitem"/>
+    <input type="text" name="newitem" bind:value={inputItem} />
     <button>Agregar</button>
   </form>
+  <div class="todo-list">
+    <ul>
+      {#if todos.length > 0}
+      {#each todos as todo}
+      <div class="todo-item">
+        <input type="checkbox" name="cb-{todo.id}" data-id={todo.id} id="" checked={todo.completed} on:click={selected}>
+        <li id="todo-{todo.id}" class="completed-{todo.completed}">{todo.name}</li>
+      </div>
+      {/each}
+      {/if}
+    </ul>
+  </div>
 </main>
 
 <style>
-
+  ul {
+    list-style: none;
+  }
+  .todo-item {
+    display: flex;
+    flex-direction: row;
+  }
+  .completed-true {
+    color: green;
+  }
+  .completed-false {
+    color: orange;
+  }
 </style>
