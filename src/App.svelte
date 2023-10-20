e8ca50c0e50541cc86558eefd8f4ee177645bec8
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-10-20 20:22:42 -0300

    Fix input vacio, handleDelete

--- src/App.svelte
+++ src/App.svelte
@@ -3,16 +3,20 @@
   let todos = JSON.parse(localStorage.getItem("todos") || "[{}]") || undefined
   function handleForm(e: any) {
     e.preventDefault()
-    console.log(inputItem)
-    let newItem = {
-      id: Math.round(Math.random() * 12412),
-      name: inputItem,
-      completed: false
+    if (inputItem.trim() !== "" ){
+      console.log(inputItem.trim())
+      // agregar id unica
+      let newItem = {
+        id: Math.round(Math.random() * 12412),
+        name: inputItem.trim(),
+        completed: false
+      }
+
+      todos = [...todos, newItem]
+      console.log(todos)
+      inputItem = ""
+      localStorage.setItem("todos", JSON.stringify(todos))
     }
-    todos = [...todos, newItem]
-    console.log(todos)
-    inputItem = ""
-    localStorage.setItem("todos", JSON.stringify(todos))
   }
   function selected(e: any) {
     console.log(e.target.dataset.id)
@@ -20,6 +24,14 @@
     let element = document.querySelector(`#todo-${id}`)
     element?.classList.toggle("completed-false")
     element?.classList.toggle("completed-true")
+  }
+
+  function handleDelete(e: any) {
+    let id = e.target.dataset.id
+    todos = todos.filter((todo: any) => {
+      return todo.id.toString() !== id
+    })
+    localStorage.setItem("todos", JSON.stringify(todos))
   }
 </script>
 
@@ -32,12 +44,13 @@
   <div class="todo-list">
     <ul>
       {#if todos.length > 0}
-      {#each todos as todo}
-      <div class="todo-item">
-        <input type="checkbox" name="cb-{todo.id}" data-id={todo.id} id="" checked={todo.completed} on:click={selected}>
-        <li id="todo-{todo.id}" class="completed-{todo.completed}">{todo.name}</li>
-      </div>
-      {/each}
+        {#each todos as todo}
+        <div class="todo-item">
+          <input type="checkbox" name="cb-{todo.id}" data-id={todo.id} id="" checked={todo.completed} on:click={selected}>
+          <li id="todo-{todo.id}" class="completed-{todo.completed}">{todo.name}</li>
+          <button data-id={todo.id} on:click={handleDelete}>Delete</button>
+        </div>
+        {/each}
       {/if}
     </ul>
   </div>
