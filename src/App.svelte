d0c84820c101d86837ddec3bfa3f8207ee6123b4
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-10-23 19:17:06 -0300

    Agregado uuid y cambio de estado on selected

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,44 @@
 <script lang="ts">
+  import { v4 as uuidv4 } from 'uuid'
   let inputItem: string = ''
   let todos = JSON.parse(localStorage.getItem("todos") || "[{}]") || undefined
   function handleForm(e: any) {
     e.preventDefault()
     if (inputItem.trim() !== "" ){
-      console.log(inputItem.trim())
-      // agregar id unica
+      // console.log(inputItem.trim())
       let newItem = {
-        id: Math.round(Math.random() * 12412),
+        id: uuidv4(),
         name: inputItem.trim(),
         completed: false
       }
-
       todos = [...todos, newItem]
-      console.log(todos)
       inputItem = ""
       localStorage.setItem("todos", JSON.stringify(todos))
     }
   }
+  $: console.log(todos)
   function selected(e: any) {
     console.log(e.target.dataset.id)
     let id = e.target.dataset.id
     let element = document.querySelector(`#todo-${id}`)
     element?.classList.toggle("completed-false")
     element?.classList.toggle("completed-true")
+    todos = todos.map((todo: any) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    localStorage.setItem("todos", JSON.stringify(todos))
+
   }
 
   function handleDelete(e: any) {
     let id = e.target.dataset.id
-    todos = todos.filter((todo: any) => {
+    let todosFiltered = todos.filter((todo: any) => {
       return todo.id.toString() !== id
     })
+    todos = todosFiltered
     localStorage.setItem("todos", JSON.stringify(todos))
   }
 </script>
@@ -38,7 +46,7 @@
 <main>
   <form on:submit={handleForm}>
     <label for="newitem">Nuevo item</label>
-    <input type="text" name="newitem" bind:value={inputItem} />
+    <input type="text" name="newitem" bind:value={inputItem} autocomplete="off"/>
     <button>Agregar</button>
   </form>
   <div class="todo-list">
