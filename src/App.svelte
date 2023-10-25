a552b0a0e5bc6c20298860f530f4dc9b1ea35471
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-10-25 19:54:56 -0300

    Edicion, fix select, delete, imagenes y estilos

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,9 @@
   let inputItem: string = ''
   let todos = JSON.parse(localStorage.getItem("todos") || "[]") || []
   let count: number 
+  let editableTodoID : string = ""
+  let editInput: string = ""
   countFilter()
-  // $: console.log(count)
-
 
   function handleForm(e: any) {
     e.preventDefault()
@@ -19,35 +19,49 @@
       inputItem = ""
       localStorage.setItem("todos", JSON.stringify(todos))
       countFilter()
-
     }
   }
-  function selected(e: any) {
-    let id = e.target.dataset.id
-    let element = document.querySelector(`#todo-${id}`)
-    element?.classList.toggle("completed-false")
-    element?.classList.toggle("completed-true")
+
+  function toggleSelection(id: string) {
     todos = todos.map((todo: any) => {
-      // sacar toString
-      if (todo.id.toString() === id) {
+      if (todo.id === id) {
         todo.completed = !todo.completed
       }
       return todo
-    })
+    }) 
     localStorage.setItem("todos", JSON.stringify(todos))
     countFilter()
-
   }
 
-  function handleDelete(e: any) {
-    let id = e.target.dataset.id
+  function handleDelete(id: string) {
     let todosFiltered = todos.filter((todo: any) => {
-      // sacar toString
-      return todo.id.toString() !== id
+      return todo.id !== id
     })
     todos = todosFiltered
     localStorage.setItem("todos", JSON.stringify(todos))
     countFilter()
+  }
+
+  function handleEdit(id: string) {
+    editableTodoID = id
+    todos.filter((todo: any) => {
+      if (todo.id === id) {
+        editInput = todo.name
+      }
+    })
+  }
+
+  function updateTodo(id: string) {
+    todos = todos.map((todo: any) => {
+      if (todo.id === id) {
+        todo.name = editInput
+      }
+      return todo
+    }) 
+    localStorage.setItem("todos", JSON.stringify(todos))
+    countFilter()
+    editInput = ""
+    editableTodoID = ""
   }
 
   function countFilter() {
@@ -57,9 +71,14 @@
 
 <main>
   <form on:submit={handleForm}>
-    <label for="newitem">Nuevo item</label>
-    <input type="text" name="newitem" bind:value={inputItem} autocomplete="off"/>
-    <button>Agregar</button>
+    <h2>Agregar Tarea</h2>
+    <div class="todo-new">
+      <input type="text" name="newitem" bind:value={inputItem} autocomplete="off" class="user-input" autofocus={true}/>
+      <button>
+        <img src="./src/assets/plus-solid.svg" alt="Boton agregar" class="add-img">    
+      </button>
+    </div>
+    <!-- <label for="newitem">Nuevo item</label> -->
   </form>
   <div class="todo-list">
     <h2>Tareas</h2>
@@ -73,9 +92,25 @@
         {#each todos as todo}
           {#if !todo.completed}
           <div class="todo-item">
-            <input type="checkbox" name="cb-{todo.id}" data-id={todo.id} id="" checked={todo.completed} on:click={selected}>
-            <li id="todo-{todo.id}" class="completed-{todo.completed}">{todo.name}</li>
-            <button data-id={todo.id} on:click={handleDelete}>Delete</button>
+
+            {#if editableTodoID === todo.id}
+            <input type="checkbox" name="todo-{todo.id}" bind:checked={todo.completed} on:click={() => toggleSelection(todo.id)} />
+            <input type="text" name="newitem" bind:value={editInput} autocomplete="off" class="user-input" autofocus={true} 
+            on:blur={() => updateTodo(todo.id)}/>
+            <button on:click={() => updateTodo(todo.id)}>
+              <img src="./src/assets/floppy-disk-solid.svg" alt="Boton guardar" class="save-img" >    
+            </button>
+
+            {:else}
+            <input type="checkbox" name="todo-{todo.id}" bind:checked={todo.completed} on:click={() => toggleSelection(todo.id)} />
+            <li id={todo.id} class="completed-{todo.completed}">{todo.name}</li>
+            <button on:click={() => handleEdit(todo.id)}>
+              <img src="./src/assets/pencil-solid.svg" alt="Boton editar" class="edit-img">    
+            </button>
+            <button on:click={() => handleDelete(todo.id)}>
+              <img src="./src/assets/trash-solid.svg" alt="Boton borrar" class="del-img">    
+            </button>
+            {/if}
           </div>
           {/if}
         {/each}
@@ -93,9 +128,24 @@
         {#each todos as todo}
           {#if todo.completed}
           <div class="todo-item">
-            <input type="checkbox" name="cb-{todo.id}" data-id={todo.id} id="" checked={todo.completed} on:click={selected}>
-            <li id="todo-{todo.id}" class="completed-{todo.completed}">{todo.name}</li>
-            <button data-id={todo.id} on:click={handleDelete}>Delete</button>
+            {#if editableTodoID === todo.id}
+            <input type="checkbox" name="todo-{todo.id}" bind:checked={todo.completed} on:click={() => toggleSelection(todo.id)} />
+            <input type="text" name="newitem" bind:value={editInput} autocomplete="off" class="user-input" autofocus={true} 
+            on:blur={() => updateTodo(todo.id)}/>
+            <button on:click={() => updateTodo(todo.id)}>
+              <img src="./src/assets/floppy-disk-solid.svg" alt="Boton guardar" class="save-img" >    
+            </button>
+
+            {:else}
+            <input type="checkbox" name="todo-{todo.id}" bind:checked={todo.completed} on:click={() => toggleSelection(todo.id)} />
+            <li id={todo.id} class="completed-{todo.completed}">{todo.name}</li>
+            <button on:click={() => handleEdit(todo.id)}>
+              <img src="./src/assets/pencil-solid.svg" alt="Boton editar" class="edit-img">    
+            </button>
+            <button on:click={() => handleDelete(todo.id)}>
+              <img src="./src/assets/trash-solid.svg" alt="Boton borrar" class="del-img">    
+            </button>
+            {/if}
           </div>
           {/if}
         {/each}
@@ -105,17 +155,53 @@
 </main>
 
 <style>
-  ul {
-    list-style: none;
-  }
-  .todo-item {
+  .todo-new {
     display: flex;
     flex-direction: row;
+    justify-content: center;
+    gap: 10px;
+  }
+  .user-input {
+    padding: 10px;
+    width: 60%;
+  }
+  h2 {
+    text-decoration: underline;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+  .todo-item {
+    display: grid;
+    grid-template-columns: 1fr 6fr 1fr 1fr;
+    column-gap: 5px;
+    max-width: 90%;
+    margin: 0 auto;
+    align-items: center;
+  }
+  .todo-item li {
+    overflow: hidden; 
+    white-space: nowrap; 
+    text-overflow: ellipsis;
+    text-align: left;
+  }
+  .todo-item button {
+    justify-self: end;
   }
   .completed-true {
-    color: green;
+    color: rgb(116, 197, 100);
+    text-decoration: line-through;
   }
   .completed-false {
-    color: orange;
+    color: rgb(255, 209, 117);
+  }
+  .del-img, .add-img, .edit-img, .save-img {
+    width: 15px;
+    fill: #ffffff;
   }
 </style>
